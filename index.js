f4a258a6c1dbd39c5e7eeb0967a948e44f537b11
lykmapipo <lallyelias87@gmail.com>
2019-01-03 10:51:51 +0300

    Add pkg information reader

--- index.js
+++ index.js
@@ -3,3 +3,24 @@
 
 /* dependencies */
 const _ = require('lodash');
+const { sync: readPkg } = require('read-pkg');
+
+
+/**
+ * @function pkg
+ * @name pkg
+ * @description read current process package information
+ * @return {Object} current process package information
+ * @author lally elias <lallyelias87@mail.com>
+ * @license MIT
+ * @since 0.1.0
+ * @version 0.1.0
+ * @private
+ * @example
+ * const { name, version } = pkg();
+ */
+exports.pkg = function pkg() {
+  const options = _.merge({}, { cwd: process.cwd() });
+  const pkg = readPkg(options);
+  return pkg;
+};
--- test/index.spec.js
+++ test/index.spec.js
@@ -3,8 +3,17 @@
 process.env.NODE_ENV = 'test';
 
 /* dependencies */
-const path = require('path');
 const { expect } = require('chai');
+const { pkg } = require('../');
 
 
-describe('common', () => {});
+describe('common', () => {
+  it('should read process package information', () => {
+    expect(pkg).to.exist;
+    expect(pkg).to.be.a('function');
+    expect(pkg.name).to.be.equal('pkg');
+    const { name, version } = pkg();
+    expect(name).to.exist;
+    expect(version).to.exist;
+  });
+});
