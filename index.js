0250f13a2379b740d18fb76f5a0f623f092149e8
lykmapipo <lallyelias87@gmail.com>
2019-01-03 10:32:01 +0300

    Initialize

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+
+/* dependencies */
+const _ = require('lodash');
---
+++ test/index.spec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+process.env.NODE_ENV = 'test';
+
+/* dependencies */
+const path = require('path');
+const { expect } = require('chai');
+
+
+describe('common', () => {});
