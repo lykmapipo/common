ae2bd331e3f6dbc4564e66def35e13ee1eb3175f
lykmapipo <lallyelias87@gmail.com>
2019-01-03 11:19:06 +0300

    Add array & object compact

--- index.js
+++ index.js
@@ -7,6 +7,42 @@ const { sync: readPkg } = require('read-pkg');
 
 
 /**
+ * @function compact
+ * @name compact
+ * @description Creates new array(or object) with all falsey values removed.
+ * The values false, null, 0, "", undefined, and NaN are falsey.
+ * @param {Array|Object} value The array(or object) to compact.
+ * @return {Object|Array} new array(or object) of filtered values.
+ * @author lally elias <lallyelias87@mail.com>
+ * @license MIT
+ * @since 0.1.0
+ * @version 0.1.0
+ * @static
+ * @public
+ * @example
+ * const a = [null, 1, "", undefined];
+ * const b = compact(a); // [ 1 ]
+ *
+ * const x = {a: 1, b: "", c: undefined};
+ * const y = compact(x); // {a: 1}
+ */
+exports.compact = function compact(value) {
+  // compact array
+  if (_.isArray(value)) {
+    return _.compact(value);
+  }
+
+  //compact object
+  if (_.isPlainObject(value)) {
+    return _.omitBy(value, function _compat(val) { return !val; });
+  }
+
+  // return value
+  return value;
+};
+
+
+/**
  * @function pkg
  * @name pkg
  * @description read current process package information
@@ -15,7 +51,8 @@ const { sync: readPkg } = require('read-pkg');
  * @license MIT
  * @since 0.1.0
  * @version 0.1.0
- * @private
+ * @static
+ * @public
  * @example
  * const { name, version } = pkg();
  */
--- test/index.spec.js
+++ test/index.spec.js
@@ -4,16 +4,34 @@ process.env.NODE_ENV = 'test';
 
 /* dependencies */
 const { expect } = require('chai');
-const { pkg } = require('../');
-
+const { compact, pkg } = require('../');
 
 describe('common', () => {
-  it('should read process package information', () => {
+  it('should read current process package information', () => {
     expect(pkg).to.exist;
     expect(pkg).to.be.a('function');
     expect(pkg.name).to.be.equal('pkg');
     const { name, version } = pkg();
     expect(name).to.exist;
     expect(version).to.exist;
+  });
+
+  it('should compact an array', () => {
+    const a = [null, 1, '', undefined];
+    const b = compact(a);
+    expect(b).to.exist;
+    expect(b).to.eql([1]);
+    b.push(2);
+    expect(a).to.not.contain(2);
+  });
+
+  it('should compact an object', () => {
+    const x = { a: null, b: 1, c: '', d: undefined };
+    const y = compact(x);
+    expect(y).to.exist;
+    expect(y).to.not.include.keys('a', 'c', 'd');
+    expect(y.b).to.exist;
+    y.e = 2;
+    expect(x.e).to.not.exist;
   });
 });
